0df8d54195e37cdb49bb6929b4403f09e4aa546b
Bastian Blokland <bastianblokland@gmail.com>
2020-04-04 20:03:26 +0300

    Only use __buildin_expect on clang and gcc

--- src/vm/internal/likely.hpp
+++ src/vm/internal/likely.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
+#if defined(__clang__) || defined(__GNUG__)
+
 // Hint to the compiler that a branch is likely to be taken.
 #define likely(x) __builtin_expect(!!(x), 1)
 
 // Hint to the compiler that a branch is unlikely to be taken.
 #define unlikely(x) __builtin_expect(!!(x), 0)
+
+#else
+
+#define likely(x) x
+
+#define unlikely(x) x
+
+#endif
