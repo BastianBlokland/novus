3231284e3af3fe0eb7e92562da2ccde8b87fe07a
Bastian Blokland <bastianblokland@gmail.com>
2020-05-09 15:59:19 +0300

    Only suppress cast-function-type in gcc on windows

--- apps/rang_include.hpp
+++ apps/rang_include.hpp
@@ -1,7 +1,13 @@
+#if defined(__GNUG__) && defined(_WIN32)
+
 // Suppress the 'cast-function-type' warning in the rang header. Reason is on MinGW this fails.
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-function-type"
+#include "rang.hpp"
+#pragma GCC diagnostic pop
+
+#else
 
 #include "rang.hpp"
 
-#pragma GCC diagnostic pop
+#endif
