a477dc8bd1bf7aa66c499b17e529f99b3a4b2d81
Bastian Blokland <bastianblokland@gmail.com>
2020-05-12 22:14:33 +0300

    Support (de)serializing novus assembly

---
+++ tests/novasm/helpers.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "backend/generator.hpp"
+#include "catch2/catch.hpp"
+#include "frontend/analysis.hpp"
+
+namespace novasm {
+
+inline auto generateAssembly(std::string input) {
+  const auto src = frontend::buildSource("test", std::nullopt, input.begin(), input.end());
+  const auto frontendOutput = frontend::analyze(src);
+  REQUIRE(frontendOutput.isSuccess());
+  return backend::generate(frontendOutput.getProg()).first;
+}
+
+#define GEN_ASM(INPUT) generateAssembly(INPUT)
+
+} // namespace novasm
---
+++ tests/novasm/serialization_test.cpp
@@ -0,0 +1,37 @@
+#include "catch2/catch.hpp"
+#include "helpers.hpp"
+#include "novasm/serialization.hpp"
+
+namespace novasm {
+
+static auto testSerializeAndDeserializeAsm(Assembly a) {
+  // Serialize.
+  auto outputString = std::string{};
+  serialize(a, std::back_inserter(outputString));
+  REQUIRE(outputString.size() > 0);
+
+  // Deserialize.
+  auto deserializedAssembly = deserialize(outputString.begin(), outputString.end());
+  REQUIRE(deserializedAssembly);
+
+  CHECK(*deserializedAssembly == a);
+}
+
+TEST_CASE("Assembly serialization", "[novasm]") {
+
+  SECTION("Basic program") {
+    testSerializeAndDeserializeAsm(GEN_ASM("act main(int i, float f) "
+                                           "  pow(i, f) + sin(f) "
+                                           "main(42, 1.337)"));
+  }
+
+  SECTION("Empty program") { testSerializeAndDeserializeAsm(GEN_ASM("")); }
+
+  SECTION("Basic program with string literals") {
+    testSerializeAndDeserializeAsm(GEN_ASM("act main(string strA, string strB) "
+                                           "  strA + strB "
+                                           "main(\"hello\", \"world\")"));
+  }
+}
+
+} // namespace novasm
