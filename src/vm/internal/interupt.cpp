e29f76dae8955c0143c911226528c46b2066dfac
Bastian Blokland <bastianblokland@gmail.com>
2020-10-25 18:33:45 +0200

    Intercept os interupts (ctrl-c)
    Adds an api 'isInteruptRequested' for applications to
    ask if an interupt has been requested. Allows applications
    to properly cleanup on interupt.

---
+++ src/vm/internal/interupt.cpp
@@ -0,0 +1,49 @@
+#include "internal/interupt.hpp"
+#include "internal/os_include.hpp"
+#include <atomic>
+#include <csignal>
+
+namespace vm::internal {
+
+static std::atomic_bool g_interruptRequested = false;
+
+auto isInterruptRequested() noexcept -> bool {
+  return g_interruptRequested.load(std::memory_order_acquire);
+}
+
+#if defined(_WIN32)
+
+auto interruptHandler(DWORD dwCtrlType) noexcept -> int {
+  switch (dwCtrlType) {
+  case CTRL_C_EVENT:
+  case CTRL_BREAK_EVENT:
+  case CTRL_CLOSE_EVENT:
+    g_interruptRequested.store(true, std::memory_order_release);
+    return true; // Indicate that we have handled the event.
+  default:
+    return false; // Indicate that we have not handled the event.
+  }
+}
+
+auto setupInterruptHandler() noexcept -> bool {
+  return SetConsoleCtrlHandler(interruptHandler, true);
+}
+
+#else // !_WIN32
+
+auto interruptHandler(int /*unused*/) noexcept -> void {
+  g_interruptRequested.store(true, std::memory_order_release);
+}
+
+auto setupInterruptHandler() noexcept -> bool {
+
+  struct sigaction actionInfo = {};
+  actionInfo.sa_handler       = interruptHandler;
+  sigemptyset(&actionInfo.sa_mask);
+
+  return sigaction(SIGINT, &actionInfo, nullptr) == 0;
+}
+
+#endif // !_WIN32
+
+} // namespace vm::internal
