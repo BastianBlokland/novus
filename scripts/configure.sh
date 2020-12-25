196adfd9b682a58c48751b1ffe3b42c595963f13
Bastian Blokland <bastianblokland@gmail.com>
2020-12-25 13:22:32 +0200

    Add coverage and sanitise options to the CMakeList
    Only supported on clang however

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -54,10 +54,14 @@ configureProj()
   local dir="${2}"
   local testsMode="${3}"
   local lintMode="${4}"
+  local sanitiseMode="${5}"
+  local coverageMode="${6}"
 
   verifyBuildTypeOption "${type}"
   verifyBoolOption "${testsMode}"
   verifyBoolOption "${lintMode}"
+  verifyBoolOption "${sanitiseMode}"
+  verifyBoolOption "${coverageMode}"
 
   # Create target directory if it doesn't exist yet.
   test -d "${dir}" || mkdir -p "${dir}"
@@ -71,7 +75,9 @@ configureProj()
     -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE="${type}" \
     -DBUILD_TESTING="${testsMode}" \
-    -DLINTING="${lintMode}"
+    -DLINTING="${lintMode}" \
+    -DSANITISE="${sanitiseMode}" \
+    -DCOVERAGE="${coverageMode}"
 
   info "Successfully configured build directory '${dir}'"
 }
@@ -84,6 +90,8 @@ printUsage()
   echo "-d,--dir      Build directory, default: 'build'"
   echo "--tests       Include compiler and runtime tests"
   echo "--lint        Enable source linter"
+  echo "--sanitise    Should santiser instrumentation be included in targets"
+  echo "--coverage    Should coverage instrumentation be included in targets"
 }
 
 # Defaults.
@@ -91,6 +99,8 @@ buildType="Release"
 buildDir="build"
 testsMode="Off"
 lintMode="Off"
+sanitiseMode="Off"
+coverageMode="Off"
 
 # Parse options.
 while [[ $# -gt 0 ]]
@@ -117,6 +127,14 @@ do
       lintMode="On"
       shift 1
       ;;
+    --sanitise)
+      sanitiseMode="On"
+      shift 1
+      ;;
+    --coverage)
+      coverageMode="On"
+      shift 1
+      ;;
     *)
       error "Unknown option '${1}'"
       printUsage
@@ -126,5 +144,11 @@ do
 done
 
 # Run configuration.
-configureProj "${buildType}" "${buildDir}" "${testsMode}" "${lintMode}"
+configureProj \
+  "${buildType}" \
+  "${buildDir}" \
+  "${testsMode}" \
+  "${lintMode}" \
+  "${sanitiseMode}" \
+  "${coverageMode}"
 exit 0
