12a88ea5a8887a277733a8b2a2665ac32ece80b3
Bastian Blokland <mail@bastian.tech>
2021-01-12 23:41:19 +0200

    backend: Update lazy get to be threadsafe

--- src/backend/internal/gen_lazy.cpp
+++ src/backend/internal/gen_lazy.cpp
@@ -6,54 +6,61 @@
 namespace backend::internal {
 
 auto genLazyGet(novasm::Assembler* asmb) -> void {
-  /* A 'lazy' object is represented by a struct with 3 fields:
-      1: Flag to indicate if the value has been computed.
-      2: Closure to call to compute the value.
-      3: Computed value (or empty)
+  /* A 'lazy' object is represented by a struct with 2 fields:
+      1: Atomic to indicate the state:
+          0 = val not computed.
+          1 = value is being computed.
+          2 = value is computed.
+      2: Closure to compute the value OR the computed value.
 
-    When executing a 'get' we check if the flag in the first field is set:
-      - If so: Return the value at field 3.
-      - If not:
+    When executing a 'get':
+      - Try to change the atomic from 0 to 1.
+      - If the old value was 0:
           * Execute a call to the closure at field 2.
-          * Store the result of the call to field 3.
-          * Set the flag at field 1.
-
-    Note: We cannot simply alias field 2 and 3 because multiple threads could call 'get' on the same
-    lazy at the same time. Thats why we need to leave the 'closure' in place even after our call
-    completes.
+          * Store the result to field 2.
+          * Change the atomic from 1 to 2.
+          * Get the resulting value from field 2.
+      - If the old value was not 0:
+          * Block until the atomic has a value of 2.
+          * Get the resulting value from field 2.
   */
 
-  auto alreadyComputedLabel = asmb->generateLabel("lazy-already-computed");
+  auto notIdleLabel      = asmb->generateLabel("lazy-get-not-idle");
+  auto returnResultLabel = asmb->generateLabel("lazy-get-return-result");
 
   // Note: we start with a single 'lazy' structure on the stack, to avoid having to store it in a
-  // stack 'variable' we duplicate it (because we access it 2 times at least).
+  // stack 'variable' we duplicate it twice (because we access it 3 times at least).
+  asmb->addDup();
   asmb->addDup();
 
-  // Load the 'flag' at field 1.
-  asmb->addStructLoadField(0);
-  asmb->addJumpIf(alreadyComputedLabel);
+  asmb->addStructLoadField(0);      // Load the atomic.
+  asmb->addAtomicCompareSwap(0, 1); // Attempt to set the atomic from 0 to 1.
+  asmb->addJumpIf(notIdleLabel);    // If the old value was not 0 then jump.
 
   // Getting here means the value has not been computed yet.
-  // Duplicate the lazy struct three times more as we need to access it three more times.
+  // Duplicate the lazy struct two times more as we need to access it two more times.
   asmb->addDup();
   asmb->addDup();
-  asmb->addDup();
 
-  // Load the closure at field 2 and call it (Note: the closure takes no arguments).
-  asmb->addStructLoadField(1);
-  asmb->addCallDyn(0, novasm::CallMode::Normal);
+  // Execute the closure and store the result.
+  asmb->addStructLoadField(1);                   // Load the closure.
+  asmb->addCallDyn(0, novasm::CallMode::Normal); // Note: The closure takes no arguments.
+  asmb->addStructStoreField(1);                  // Store the computed value at field 2.
 
-  // Store computed value in field 3.
-  asmb->addStructStoreField(2);
+  // Set the atomic to 2.
+  asmb->addStructLoadField(0);      // Load the atomic
+  asmb->addAtomicCompareSwap(1, 2); // Set the atomic from 0 to 1 (we know it will succeed).
+  asmb->addPop();                   // Ignore the result of the compare and swap.
+  asmb->addJump(returnResultLabel); // Skip over the waiting.
 
-  // Set the flag in field 1 to 'true'.
-  // Note: we still had a lazy struct on the stack due to the dup's we did before)
-  asmb->addLoadLitInt(1);
-  asmb->addStructStoreField(0);
+  // Wait for the value to be available.
+  asmb->label(notIdleLabel);
+  asmb->addStructLoadField(0); // Load the atomic.
+  asmb->addAtomicBlock(2);     // Wait for the atomic to have the value 2.
 
-  // Load the computed value at field 3.
-  asmb->label(alreadyComputedLabel);
-  asmb->addStructLoadField(2);
+  // Return.
+  asmb->label(returnResultLabel);
+  asmb->addStructLoadField(1); // Load the computed value.
 }
 
 } // namespace backend::internal
