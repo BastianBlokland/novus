179effe19e71fe80008e5bc939c3d9252442f375
Bastian Blokland <mail@bastian.tech>
2025-11-16 13:39:37 +0200

    nix: Inital nix dev shell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Novus Nix Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+  };
+
+  outputs = { nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    llvmPkg = pkgs.llvmPackages_21;
+  in
+  {
+    devShells.${system} = rec {
+
+      llvm = (pkgs.mkShellNoCC.override { stdenv = llvmPkg.stdenv; }) {
+        packages = [
+          pkgs.clang-tools
+
+          llvmPkg.lld
+          llvmPkg.libcxx
+          llvmPkg.lldb
+          llvmPkg.clang
+
+          pkgs.cmake
+          pkgs.gnumake
+        ];
+      };
+
+      default = llvm;
+    };
+  };
+}
