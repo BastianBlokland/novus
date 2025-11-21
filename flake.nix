eae74ebbe837bcb54bd829bbda51289e7d0bb51b
Bastian Blokland <mail@bastian.tech>
2025-11-21 15:39:42 +0200

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -5,31 +5,33 @@
     nixpkgs.url = "nixpkgs/nixos-25.05";
   };
 
-  outputs = { nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    llvmPkg = pkgs.llvmPackages_21;
-  in
-  {
-    devShells.${system} = rec {
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      llvmPkg = pkgs.llvmPackages_21;
+    in
+    {
+      devShells.${system} = rec {
 
-      llvm = (pkgs.mkShellNoCC.override { stdenv = llvmPkg.stdenv; }) {
-        packages = [
-          pkgs.clang-tools
+        llvm = (pkgs.mkShellNoCC.override { stdenv = llvmPkg.stdenv; }) {
+          packages = [
+            pkgs.nixfmt-rfc-style
+            pkgs.clang-tools
 
-          llvmPkg.lld
-          llvmPkg.libcxx
-          llvmPkg.lldb
-          llvmPkg.clang
-          llvmPkg.bintools
+            llvmPkg.lld
+            llvmPkg.libcxx
+            llvmPkg.lldb
+            llvmPkg.clang
+            llvmPkg.bintools
 
-          pkgs.cmake
-          pkgs.gnumake
-        ];
+            pkgs.cmake
+            pkgs.gnumake
+          ];
+        };
+
+        default = llvm;
       };
-
-      default = llvm;
     };
-  };
 }
